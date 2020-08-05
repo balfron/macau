9a4ddbae9f1e90bd37bc30f0313ac07ded3d129b
balfron <40870085+balfron@users.noreply.github.com>
2020-08-05 15:41:52 +0100

    Updates

---
+++ precache-manifest.d92bb94938423113a525bd69068630d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a400ae61bb430520f70acc11f4bba75",
+    "url": "/macau/index.html"
+  },
+  {
+    "revision": "b9fc52e6cfb64745fc89",
+    "url": "/macau/static/css/main.6c984903.chunk.css"
+  },
+  {
+    "revision": "1c7809ffc179d08ca66e",
+    "url": "/macau/static/js/2.79a02463.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/macau/static/js/2.79a02463.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9fc52e6cfb64745fc89",
+    "url": "/macau/static/js/main.20858c93.chunk.js"
+  },
+  {
+    "revision": "4213700f4dac4d5ed4c7",
+    "url": "/macau/static/js/runtime-main.125f7b21.js"
+  }
+]);
